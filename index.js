87343873df92f2d15299646a46c4a6fe363f6a8d
Xian-Wu2021 <xianwu.iris@gmail.com>
2021-04-28 22:57:50 +0800

    change

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-dddd
+const express = require("express");
+const path = require("path");
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log("Sever started (http://localhost:3000/) !");
+})
+
+app.get("/", (req, res) => {
+    //res.send ("Hello word...");
+    res.render("index");
+})
+
+//5. Use views in Express
+
+app.set("view engine", "ejs");
+
+
+
+app.set("views", __dirname + "/views");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/about", (req, res) => {
+    res.render("about");
+})
+
+app.get("/data", (req, res) => {
+    const test = {
+        title: "Test",
+        items: ["one", "two", "three"]
+    };
+    res.render("data", { model: test });
+});
